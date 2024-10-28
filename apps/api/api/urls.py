21b0b5ebbc07c1e64e70bba9072bb923c685ba39
Harris Pittinsky <harris@pittinsky.com>
2024-10-28 14:02:04 -0400

    Add password reset functionality to django admin

--- apps/api/api/urls.py
+++ apps/api/api/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.conf import settings
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import include, path
 from django.views import generic
 
@@ -25,6 +26,34 @@ admin.site.site_header = project_name
 admin.site.site_title = project_name
 
 urlpatterns = [
+    path(
+        "admin/password_reset/",
+        auth_views.PasswordResetView.as_view(
+            extra_context={"site_header": admin.site.site_header}
+        ),
+        name="admin_password_reset",
+    ),
+    path(
+        "admin/password_reset/done/",
+        auth_views.PasswordResetDoneView.as_view(
+            extra_context={"site_header": admin.site.site_header}
+        ),
+        name="password_reset_done",
+    ),
+    path(
+        "reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            extra_context={"site_header": admin.site.site_header}
+        ),
+        name="password_reset_confirm",
+    ),
+    path(
+        "reset/done/",
+        auth_views.PasswordResetCompleteView.as_view(
+            extra_context={"site_header": admin.site.site_header}
+        ),
+        name="password_reset_complete",
+    ),
     path('admin/', admin.site.urls),
     path('', generic.RedirectView.as_view(pattern_name='admin:login'), name='home'),
     path('api/auth/', include('rest_framework.urls')),
