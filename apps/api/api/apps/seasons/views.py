f15fde175cbb11a12c77e5acec45f0efab8187a5
Harris Pittinsky <harris@pittinsky.com>
2024-12-01 15:26:14 -0500

    Add season registration read only endpoints

--- apps/api/api/apps/seasons/views.py
+++ apps/api/api/apps/seasons/views.py
@@ -1,7 +1,7 @@
 from common.views import BaseModelReadOnlyViewSet
-from .filtersets import SeasonFilterSet
-from .models import Season
-from .serializers import SeasonReadOnlySerializer
+from .filtersets import SeasonFilterSet, SeasonRegistrationFilterSet
+from .models import Season, SeasonRegistration
+from .serializers import SeasonReadOnlySerializer, SeasonRegistrationReadOnlySerializer
 
 
 class SeasonViewSet(BaseModelReadOnlyViewSet):
@@ -10,3 +10,12 @@ class SeasonViewSet(BaseModelReadOnlyViewSet):
     ordering = ('start',)
     ordering_fields = ('start', 'end',)
     filterset_class = SeasonFilterSet
+
+
+class SeasonRegistrationViewSet(BaseModelReadOnlyViewSet):
+    queryset = SeasonRegistration.objects.all().select_related('user', 'season', 'team')
+    serializer_class = SeasonRegistrationReadOnlySerializer
+    ordering = ('season', 'team', 'user')
+    ordering_fields = ('season', 'team',)
+    search_fields = ('user__username', 'user__email', 'user__first_name', 'user__last_name', 'team__name',)
+    filterset_class = SeasonRegistrationFilterSet
