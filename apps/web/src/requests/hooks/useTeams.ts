8b7ec9026574f2ff9a55076d379f91ec9a4032e0
Harris Pittinsky <harris@pittinsky.com>
2024-12-01 17:45:46 -0500

    Revert use team hook change

--- apps/web/src/requests/hooks/useTeams.ts
+++ apps/web/src/requests/hooks/useTeams.ts
@@ -3,7 +3,7 @@ import { usePlaceholder, useRequest } from "@/requests/hooks";
 export const useTeams = ({ short_name }: { short_name?: string }) => {
   const { data, placeholder, loading, error, ...rest } = usePlaceholder(
     useRequest({
-      route: short_name ? `teams/${short_name}` : "teams",
+      route: short_name ? `team/${short_name}` : "teams",
     })
   );
   return {
