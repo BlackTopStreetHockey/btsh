35175622f4a3aece930595179e05fff2076cfc47
Justin Michaliga <justin.michaliga@gmail.com>
2024-12-05 22:06:47 -0500

    feat: update some rosters/schedule

--- apps/web/src/app/teams/[team]/components/team-roster.tsx
+++ apps/web/src/app/teams/[team]/components/team-roster.tsx
@@ -12,12 +12,12 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { ArrowUp } from "lucide-react";
+import { ArrowUp, Star } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
 import { useRoster } from "@/hooks/requests/useRoster";
 
-type SortField = "name" | "position" | "gender" | "gp" | "goals" | "gpg";
+type SortField = "full_name" | "position" | "gender" | "gp" | "goals" | "gpg";
 type SortDirection = "asc" | "desc";
 
 export function TeamRoster({
@@ -29,7 +29,7 @@ export function TeamRoster({
   teamId: string;
   seasonYear: string;
 }) {
-  const [sortField, setSortField] = useState<SortField>("name");
+  const [sortField, setSortField] = useState<SortField>("full_name");
   const [sortDirection, setSortDirection] = useState<SortDirection>("asc");
   const { roster } = useRoster({ seasonId, teamId });
 
@@ -92,53 +92,59 @@ export function TeamRoster({
       </CardHeader>
       <CardContent>
         <ScrollArea className="h-[500px]">
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead>
-                <SortableHeader field="name">Name</SortableHeader>
-              </TableHead>
-              <TableHead>
-                <SortableHeader field="position">Position</SortableHeader>
-              </TableHead>
-              <TableHead>
-                <SortableHeader field="gender">Gender</SortableHeader>
-              </TableHead>
-              <TableHead>
-                <SortableHeader field="gp">GP</SortableHeader>
-              </TableHead>
-              <TableHead className="text-right">
-                <SortableHeader field="goals">Goals</SortableHeader>
-              </TableHead>
-              <TableHead className="text-right">
-                <SortableHeader field="gpg">GPG</SortableHeader>
-              </TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {sortedPlayers.map((player) => (
-              <TableRow key={player.user.id}>
-                <TableCell>{player.user.full_name}</TableCell>
-                <TableCell className="text-center uppercase text-xs">
-                  {player.is_captain ? "⭐️ " : ""}{" "}
-                  {player.position.substring(0, 1)}
-                </TableCell>
-                <TableCell
-                  className={`text-center ${player.user.gender === "male" ? "text-blue-600" : "text-pink-600"}`}
-                >
-                  {player.user.gender === "male" ? "M" : "F"}
-                </TableCell>
-                <TableCell className="text-center">{player.gp}</TableCell>
-                <TableCell className="text-center">{player.goals}</TableCell>
-                <TableCell className="text-center">
-                  {player.goals && player.gp
-                    ? Math.round((player.goals / player.gp) * 100) / 100
-                    : 0}
-                </TableCell>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="w-[150px]">
+                  <SortableHeader field="full_name">Name</SortableHeader>
+                </TableHead>
+                <TableHead>
+                  <SortableHeader field="position">Position</SortableHeader>
+                </TableHead>
+                <TableHead>
+                  <SortableHeader field="gender">Gender</SortableHeader>
+                </TableHead>
+                <TableHead>
+                  <SortableHeader field="gp">GP</SortableHeader>
+                </TableHead>
+                <TableHead className="text-right">
+                  <SortableHeader field="goals">Goals</SortableHeader>
+                </TableHead>
+                <TableHead className="text-right">
+                  <SortableHeader field="gpg">GPG</SortableHeader>
+                </TableHead>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
+            </TableHeader>
+            <TableBody>
+              {sortedPlayers.map((player) => (
+                <TableRow key={player.user.id}>
+                  <TableCell className="flex w-[150px] items-center gap-1">
+                    {player.user.full_name}
+                    {player.is_captain ? (
+                      <Star size={16} className="text-yellow-500" />
+                    ) : (
+                      ""
+                    )}
+                  </TableCell>
+                  <TableCell className="text-center uppercase text-xs">
+                    {player.position.substring(0, 1)}
+                  </TableCell>
+                  <TableCell
+                    className={`text-center ${player.user.gender === "male" ? "text-blue-600" : "text-pink-600"}`}
+                  >
+                    {player.user.gender === "male" ? "M" : "F"}
+                  </TableCell>
+                  <TableCell className="text-center">{player.gp}</TableCell>
+                  <TableCell className="text-center">{player.goals}</TableCell>
+                  <TableCell className="text-center">
+                    {player.goals && player.gp
+                      ? Math.round((player.goals / player.gp) * 100) / 100
+                      : 0}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
         </ScrollArea>
       </CardContent>
     </Card>
--- apps/web/src/app/teams/[team]/components/team-schedule.tsx
+++ apps/web/src/app/teams/[team]/components/team-schedule.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import { ScrollArea } from "@/components/ui/scroll-area";
 
 import { useSchedule } from "@/hooks/requests/useSchedule";
-
+import { formatTime } from "@/lib/dates";
 export function TeamSchedule({
   seasonId = "2",
   teamId = "2",
@@ -58,7 +58,10 @@ export function TeamSchedule({
                         key={game.id}
                         className="flex flex-col justify-between"
                       >
-                        <p className="text-xs text-gray-500">{gameDay.day}</p>
+                        <p className="text-xs text-gray-500">
+                          {gameDay.day} - {formatTime(game.start.toString())} -{" "}
+                          {game.get_court_display}
+                        </p>
                         <div className="flex items-center space-x-2">
                           <Image
                             src={
@@ -74,14 +77,28 @@ export function TeamSchedule({
                             width={30}
                             height={30}
                           />
-                          <p className="font-semibold">
+                          <p className="font-semibold leading-2">
                             {isHomeGame
                               ? "vs " + game.away_team.name
                               : "@ " + game.home_team.name}
                           </p>
-                        </div>
 
-                        <span className={`font-bold`}>{game.result}</span>
+                          {game.away_team_num_goals &&
+                            game.home_team_num_goals && (
+                              <span
+                                className={`font-bold text-sm ${
+                                  isHomeGame &&
+                                  game.home_team_num_goals >
+                                    game.away_team_num_goals
+                                    ? "text-red-500"
+                                    : "text-green-500"
+                                }`}
+                              >
+                                ({game.away_team_num_goals} &ndash;{" "}
+                                {game.home_team_num_goals})
+                              </span>
+                            )}
+                        </div>
                       </div>
                     );
                   })}
