fcbd4f87901566d38c262a905b4e32b137354117
Campbell Weaver <campbellweaver@mac.com>
2024-12-08 12:28:52 -0500

    logo tweaks

--- apps/web/src/components/navigation/btsh-logo.tsx
+++ apps/web/src/components/navigation/btsh-logo.tsx
@@ -5,8 +5,8 @@ import Link from "next/link";
 export default function BTSHLogo({ row }: { row?: boolean }) {
   return (
     <Link href='/'>
-      <div className={`flex ${row ? 'flex-row' : 'flex-col'} gap-2 items-center`}>
-        <Image src="/btsh-head.svg" alt="BTSH" width={36} height={36} />
+      <div className={`flex ${row ? 'flex-row' : 'flex-col'} gap-0 items-center`}>
+        <Image src="/btsh-head.svg" alt="BTSH" width={40} height={40} />
         <h2 className="text-lg font-bold">BTSH</h2>
       </div>
     </Link>
