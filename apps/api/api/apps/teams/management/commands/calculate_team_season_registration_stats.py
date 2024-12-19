867b74c6e63c89de78bd181620d22d8ff9a40bf2
Harris Pittinsky <harris@pittinsky.com>
2024-12-19 16:31:19 -0500

    Add debug mode, add season admin action to recalculate season stats

--- apps/api/api/apps/teams/management/commands/calculate_team_season_registration_stats.py
+++ apps/api/api/apps/teams/management/commands/calculate_team_season_registration_stats.py
@@ -10,5 +10,5 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         start = datetime.datetime.now()
-        calculate_team_season_registration_stats()
+        calculate_team_season_registration_stats(debug=True)
         print(f'\nTook: {datetime.datetime.now() - start}')
