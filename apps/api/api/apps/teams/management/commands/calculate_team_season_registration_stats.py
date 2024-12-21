eb1e4349dc906d463129815c9bc461950ffe1986
Harris Pittinsky <harris@pittinsky.com>
2024-12-21 13:47:18 -0500

    Add goals for, goals against, goal diff fields, DRY out computations for game stats

--- apps/api/api/apps/teams/management/commands/calculate_team_season_registration_stats.py
+++ apps/api/api/apps/teams/management/commands/calculate_team_season_registration_stats.py
@@ -2,6 +2,7 @@ import datetime
 
 from django.core.management import BaseCommand
 
+from games.models import Game
 from ...utils import calculate_team_season_registration_stats
 
 
@@ -10,5 +11,5 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         start = datetime.datetime.now()
-        calculate_team_season_registration_stats(debug=True)
+        calculate_team_season_registration_stats(game_type=Game.REGULAR, debug=True)
         print(f'\nTook: {datetime.datetime.now() - start}')
