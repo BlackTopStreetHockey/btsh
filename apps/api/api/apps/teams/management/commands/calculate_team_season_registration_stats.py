a4ed45ae88266de07951a6bbc39df0349133f90c
Harris Pittinsky <harris@pittinsky.com>
2024-12-25 17:56:11 -0500

    Remove duplicate time tracking

--- apps/api/api/apps/teams/management/commands/calculate_team_season_registration_stats.py
+++ apps/api/api/apps/teams/management/commands/calculate_team_season_registration_stats.py
@@ -1,5 +1,3 @@
-import datetime
-
 from django.core.management import BaseCommand
 
 from games.models import Game
@@ -10,6 +8,4 @@ class Command(BaseCommand):
     help = 'Calculate team season registration stats'
 
     def handle(self, *args, **options):
-        start = datetime.datetime.now()
         calculate_team_season_registration_stats(game_type=Game.REGULAR, debug=True)
-        print(f'\nTook: {datetime.datetime.now() - start}')
