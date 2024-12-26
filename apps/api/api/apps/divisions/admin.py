b581b9f2c21ab04e5e52738ba2c75f8353300113
Harris Pittinsky <harris@pittinsky.com>
2024-12-26 10:13:05 -0500

    Add import/export resource classes for divisions

--- apps/api/api/apps/divisions/admin.py
+++ apps/api/api/apps/divisions/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
 
 from common.admin import BaseModelAdmin
+from common.resources import BaseModelResource
 from .models import Division
+
+
+class DivisionImportResource(BaseModelResource):
+    class Meta(BaseModelResource.Meta):
+        model = Division
+        fields = ('id', 'name',)
+
+
+class DivisionExportResource(BaseModelResource):
+    class Meta(BaseModelResource.Meta):
+        model = Division
+        fields = BaseModelResource.Meta.fields + ('name',)
 
 
 @admin.register(Division)
@@ -9,3 +22,5 @@ class DivisionAdmin(BaseModelAdmin):
     list_display = ('name',)
     search_fields = ('name',)
     ordering = ('name',)
+    import_resource_classes = [DivisionImportResource]
+    export_resource_classes = [DivisionExportResource]
